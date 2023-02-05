6d7a736a76e245d778f99b531c4fe18ec20bedf4
tenisaname <60942207+tenisaname@users.noreply.github.com>
2023-02-05 21:55:23 +0500

    main functionality of application

--- main.py
+++ main.py
@@ -11,33 +11,46 @@ my_font = customtkinter.CTkFont(size=56)
 hour = 59
 minutes = 60
 def clock():
-    global hour 
-    global minutes
-    if (minutes == 0):
-        hour = hour  - 1
-        minutes = 60
-    minutes = minutes - 1
-    label.configure(text= str(hour) + ":" + str(minutes))
-    label.after(1000, clock)
-    if (hour == 0):
-        label.configure(text = "Focus is over")
+    if running:
+        global hour 
+        global minutes
+        if (minutes == 0):
+            hour = hour  - 1
+            minutes = 60
+        minutes = minutes - 1
+        label.configure(text= str(hour) + ":" + str(minutes))
+        label.after(1000, clock)
+        if (hour == 0):
+            label.configure(text = "Focus is over")
 
 def button_event():
     clock()
     button.configure(text="Stop Focus",command=stop)
 
 def stop():
-    label.configure(text="60:00",font=my_font)
-    
+    global running
+    running = False
+    button.configure(text="Start Focus",command=start)
+def start():
+    global running
+    running = True
+    button_event()
 
+def restart():
+    global hour 
+    global minutes
+    hour = 59
+    minutes = 60
+    label.configure(text="60:00")
+    
 label = customtkinter.CTkLabel(master=app, text="60:00",font=my_font)
 label.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
 label.pack(pady=40)
 
-button = customtkinter.CTkButton(master=app, text="Start Focus", command=button_event)
-
+button = customtkinter.CTkButton(master=app, text="Start Focus", command=start)
+button2 = customtkinter.CTkButton(master=app, text="Retry", command=restart)
 button.pack(padx=20, pady=10)
-
+button2.pack(padx=10, pady=10)
 app.mainloop()
 
 
